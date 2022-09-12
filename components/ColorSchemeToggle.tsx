7d9c669e8bf6310e7e4a6c45fa06904636fab1aa
KaiNakamura <kaihnakamura@gmail.com>
2022-09-11 21:13:46 -0400

    Fix formatting

--- components/ColorSchemeToggle.tsx
+++ components/ColorSchemeToggle.tsx
@@ -1,5 +1,5 @@
-import { ActionIcon, Group, useMantineColorScheme } from '@mantine/core';
-import { IconSun, IconMoonStars } from '@tabler/icons';
+import { ActionIcon, Group, useMantineColorScheme } from "@mantine/core";
+import { IconSun, IconMoonStars } from "@tabler/icons";
 
 export function ColorSchemeToggle() {
 	const { colorScheme, toggleColorScheme } = useMantineColorScheme();
@@ -10,11 +10,11 @@ export function ColorSchemeToggle() {
 			size="xl"
 			sx={(theme) => ({
 				backgroundColor:
-					theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-				color: theme.colorScheme === 'dark' ? theme.colors.yellow[4] : theme.colors.blue[6],
+					theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
+				color: theme.colorScheme === "dark" ? theme.colors.yellow[4] : theme.colors.blue[6],
 			})}
 		>
-			{colorScheme === 'dark' ? (
+			{colorScheme === "dark" ? (
 				<IconSun size={20} stroke={1.5} />
 			) : (
 				<IconMoonStars size={20} stroke={1.5} />
