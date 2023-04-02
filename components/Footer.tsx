5564f2c07075a94c6e807313c64ad6e19e0ea7e3
KaiNakamura <kaihnakamura@gmail.com>
2023-04-02 16:53:38 -0400

    Add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,20 @@
+import { Link } from "@components/Link";
+
 export default function Footer() {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className="bg-gray-dark absolute w-full h-16 mt-16 flex justify-evenly items-center">
+      <Link
+        className="text-white hover:underline"
+        href="https://github.com/KaiNakamura/kainakamura.com"
+      >
+        Source Code
+      </Link>
+      <Link
+        className="text-white hover:underline"
+        href="https://github.com/KaiNakamura/kainakamura.com/issues"
+      >
+        Report Bug
+      </Link>
     </footer>
   );
 }
