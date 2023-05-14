8eb7a04f366267ee29fc4bdbc5c90719b6cd9c09
KaiNakamura <kaihnakamura@gmail.com>
2023-05-14 12:20:18 -0700

    Fix touch action on hero section arrow

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,28 +3,30 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="relative overflow-hidden select-none touch-none">
-      <Boids />
-      <section className="width-screen min-h-[65vh] bg-gray flex flex-col justify-center items-center">
-        <div className="flex justify-center items-center gap-4 py-8 sm:gap-8 md:gap-10 lg:gap-12 xl:gap-16 relative z-10 pointer-events-none">
-          <div>
-            <h1 className="text-white font-bold leading-none text-[10vw] xl:text-9xl">
-              Hi, I&apos;m Kai
-            </h1>
-            <h2 className="text-blue leading-none text-[3.75vw] xl:text-5xl">
-              Software Dev / Student
-            </h2>
+    <div className="relative overflow-hidden select-none">
+      <div className="touch-none">
+        <Boids />
+        <section className="width-screen min-h-[65vh] bg-gray flex flex-col justify-center items-center">
+          <div className="flex justify-center items-center gap-4 py-8 sm:gap-8 md:gap-10 lg:gap-12 xl:gap-16 relative z-10 pointer-events-none">
+            <div>
+              <h1 className="text-white font-bold leading-none text-[10vw] xl:text-9xl">
+                Hi, I&apos;m Kai
+              </h1>
+              <h2 className="text-blue leading-none text-[3.75vw] xl:text-5xl">
+                Software Dev / Student
+              </h2>
+            </div>
+            <Image
+              src="/kai-nakamura.jpg"
+              alt="Kai Nakamura"
+              width={320}
+              height={320}
+              className="rounded-full max-w-[25vw] xl:max-w-[320px]"
+            />
           </div>
-          <Image
-            src="/kai-nakamura.jpg"
-            alt="Kai Nakamura"
-            width={320}
-            height={320}
-            className="rounded-full max-w-[25vw] xl:max-w-[320px]"
-          />
-        </div>
-        <div className="min-h-[75px]"></div>
-      </section>
+          <div className="min-h-[75px]"></div>
+        </section>
+      </div>
       <div className="width-screen h-[75px] mt-[calc(-75px+2px)]">
         <div className="absolute bg-transparent w-0 h-0 left-[calc(50%-75px+1px)] border-solid border-b-[75px] border-b-white border-r-[75px] border-r-transparent"></div>
         <div className="absolute bg-transparent w-0 h-0 left-[calc(50%-1px)] border-solid border-b-[75px] border-b-white border-l-[75px] border-l-transparent"></div>
