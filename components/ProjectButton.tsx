d5233c2a5add457d11ed328124f041dedcfb7ce0
KaiNakamura <kaihnakamura@gmail.com>
2023-10-14 04:55:34 -0400

    Fix link

--- components/ProjectButton.tsx
+++ components/ProjectButton.tsx
@@ -9,7 +9,7 @@ export interface ProjectButtonProps {
 export default function ProjectButton({ href, children }: ProjectButtonProps) {
   return (
     <button className="bg-green hover:bg-green-dark font-bold rounded-full px-2 py-1.5 sm:px-3 sm:py-2 md:px-4 md:py-2 text-sm sm:text-base md:text-lg">
-      <Link href={href} isInternalLink={false} className="text-white">
+      <Link href={href} internal={false} className="text-white">
         {children}
       </Link>
     </button>
